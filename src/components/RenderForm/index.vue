af4abae1cf70e3bea926b881e544e6b8ed8c838a
caixiaofeng <1941338475@qq.com>
2024-12-22 11:23:16 +0800

    初始提交

---
+++ src/components/RenderForm/index.vue
@@ -0,0 +1,86 @@
+<script setup lang="ts">
+import { type FormField, FormRender } from '@xfc/vue3-form-render'
+import type { FormInstance, FormItemRule } from 'element-plus'
+
+const props = defineProps<{
+  field: FormField
+  formData: Recordable
+}>()
+const emits = defineEmits<{
+  (e: 'submit', formData: Recordable): void
+  (e: 'reset', formData: Recordable): void
+}>()
+const formRef = ref<FormInstance>()
+const buildRule = (field: FormField) => {
+  const { props, rules } = field
+  let ruleList: FormItemRule[] = []
+  if (Array.isArray(rules)) {
+    ruleList.push(
+      ...rules.map((rule) => {
+        return {
+          pattern: rule.pattern && new RegExp(rule.pattern),
+          message: `请输入正确的${rule.message}`
+        }
+      })
+    )
+  }
+  if (field.required !== undefined) {
+    const required: FormItemRule = {
+      required: field.required || false,
+      message: props.placeholder
+    }
+    if (Array.isArray(field.value)) {
+      required.type = 'array'
+      required.message = `请至少选择一个${field.label}`
+      required.trigger = 'change'
+    }
+    required.message === undefined && (required.message = `${field.label}不能为空`)
+    ruleList.push(required)
+  }
+  return ruleList
+}
+const submitForm = () => {
+  return new Promise<Recordable>((resolve, reject) => {
+    if (!formRef.value) {
+      reject()
+      return
+    }
+    formRef.value?.validate((valid, fields) => {
+      if (valid) {
+        emits('submit', props.formData)
+        resolve(props.formData)
+      } else {
+        reject(fields)
+      }
+    })
+  })
+}
+const resetForm = () => {
+  if (!formRef.value) return
+  formRef.value.resetFields()
+  emits('reset', props.formData)
+}
+defineExpose({
+  submitForm,
+  resetForm,
+  buildRule
+})
+</script>
+
+<template>
+  <FormRender ref="formRef" :model="formData" :formData="formData" :field="field" v-bind="$attrs">
+    <template #formItem="{ render, field }">
+      <el-form-item
+        v-if="!field.hidden"
+        :prop="field.id"
+        :label="field.label"
+        :required="field.required"
+        :rules="buildRule(field)"
+      >
+        <component :is="render"></component>
+      </el-form-item>
+    </template>
+  </FormRender>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ src/views/form-design/editor-body/index.tsx
@@ -0,0 +1,251 @@
+import type { PropType, Ref, VNode } from 'vue'
+import Draggable from '@xfc/vue3-draggable'
+import { CopyDocument, Delete, Rank } from '@element-plus/icons-vue'
+import './index.scss'
+import { cloneDeep } from 'lodash-es'
+import { type FormField, JsonRender } from '@xfc/vue3-form-render'
+
+export default defineComponent({
+  props: {
+    field: {
+      type: Object as PropType<FormField>,
+      required: true
+    }
+  },
+  setup(props) {
+    interface FieldContext {
+      field: FormField
+      index: number
+      list: FormField[]
+      parent: FormField
+    }
+
+    const { activeData } = inject<{
+      activeData: Ref<FormField>
+    }>('formDesign')!
+    const renders: Recordable<(context: FieldContext) => VNode> = {
+      Form: (context: FieldContext): VNode => {
+        const { field } = context
+        const active = activeData.value?.id === field.id
+        const className = ['editor-form', { 'is-active': active }]
+        const child = field.children?.map((item, index) =>
+          renderItem({ field: item, index, list: field.children ?? [], parent: field })
+        )
+        return (
+          <el-form
+            label-position={field.props.labelPosition}
+            disabled={field.readonly}
+            label-width={`${field.props.labelWidth}px`}
+            size={field.props.size}
+            class={className}
+            onClick={(event: MouseEvent) => {
+              activeData.value = field
+              event.stopPropagation()
+            }}
+          >
+            <Draggable
+              modelValue={field.children ?? []}
+              onUpdate:modelValue={(val: FormField[]) => (field.children = val)}
+              options={{
+                group: 'componentsGroup',
+                handle: '.pointer-move',
+                animation: 340,
+                clone: false
+              }}
+              onAdd={(element) => (activeData.value = element)}
+              class="drawing-board p5px"
+            >
+              {child}
+            </Draggable>
+          </el-form>
+        )
+      },
+      formItem: (context: FieldContext): VNode => {
+        const { field } = context
+        const active = activeData.value?.id === field.id
+        const className = ['drawing-item', { active: active }, { is_hidden: field.hidden }]
+        const child = renderChildren(field)
+        return (
+          <div
+            class={className}
+            onClick={(event: MouseEvent) => {
+              activeData.value = field
+              event.stopPropagation()
+            }}
+          >
+            <el-form-item label={field.label} required={field.required} class="field-wrapper">
+              <JsonRender
+                key={field.props.key}
+                field={field}
+                modelValue={field.value}
+                onUpdate:modelValue={(val) => {
+                  if (field.readonly) return
+                  field.value = val
+                }}
+              >
+                {child}
+              </JsonRender>
+            </el-form-item>
+            {active ? renderTool(context) : null}
+          </div>
+        )
+      },
+      container: (context: FieldContext): VNode => {
+        const { field } = context
+        const active = activeData.value?.id === field.id
+        const className = ['drawing-item', { active: active }, { is_hidden: field.hidden }]
+        const containers: string[] = ['Tab', 'Collapse', 'Row']
+        const child = containers.includes(field.name) ? renderInner(field) : renderChildren(field)
+        return (
+          <div
+            class={className}
+            onClick={(event: MouseEvent) => {
+              activeData.value = field
+              event.stopPropagation()
+            }}
+          >
+            <div class="field-wrapper">
+              <JsonRender
+                key={field.props.key}
+                field={field}
+                modelValue={field.value}
+                onUpdate:modelValue={(val) => {
+                  field.value = val
+                }}
+              >
+                {child}
+              </JsonRender>
+            </div>
+            {active ? renderTool(context) : null}
+          </div>
+        )
+      }
+    }
+    const tools: Recordable<(context: FieldContext) => VNode> = {
+      move: (_) => {
+        return (
+          <div class="pointer-move">
+            <el-icon size={15}>
+              <Rank />
+            </el-icon>
+          </div>
+        )
+      },
+      copy: (context) => {
+        const { field, index, list } = context
+        return (
+          <div
+            onClick={(event: MouseEvent) => {
+              const clone = cloneDeep(field)
+              const generateId = (clone: FormField) => {
+                clone.id = `field_${Math.random().toString(36).substring(2, 7)}`
+                clone.children?.forEach((el) => generateId(el))
+              }
+              generateId(clone)
+              list.splice(index + 1, 0, clone)
+              activeData.value = clone
+              event.stopPropagation()
+            }}
+          >
+            <el-icon size={14}>
+              <CopyDocument />
+            </el-icon>
+          </div>
+        )
+      },
+      del: (context) => {
+        const { index, list, parent } = context
+        return (
+          <div
+            onClick={(event: MouseEvent) => {
+              list.splice(index, 1)
+              const len = list.length
+              if (len > 0) {
+                if (index < len) {
+                  activeData.value = list[index]
+                } else {
+                  activeData.value = list[index - 1]
+                }
+              } else {
+                activeData.value = parent
+              }
+              event.stopPropagation()
+            }}
+          >
+            <el-icon size={14}>
+              <Delete />
+            </el-icon>
+          </div>
+        )
+      }
+    }
+    const renderTool = (context: FieldContext) => {
+      return [
+        tools.move(context),
+        <div class="pointer-wrapper">
+          {tools.copy(context)}
+          {tools.del(context)}
+        </div>
+      ]
+    }
+    const renderItem = (context: FieldContext): VNode => {
+      const { field } = context
+      const render = renders[field.name] || renders[field.type]
+      if (render) {
+        return render(context)
+      }
+      return <span style={{ color: 'red' }}>{`No rendering found for ${field.id}`}</span>
+    }
+    const renderChildren = (field: FormField): VNode | undefined => {
+      if (field.children) {
+        const items = field.children.map((item, index) =>
+          renderItem({ field: item, index, list: field.children ?? [], parent: field })
+        )
+        return (
+          <Draggable
+            modelValue={field.children}
+            onUpdate:modelValue={(val: FormField[]) => (field.children = val)}
+            options={{
+              group: 'componentsGroup',
+              handle: '.pointer-move',
+              animation: 340
+            }}
+            onAdd={(element) => (activeData.value = element)}
+            class="widget-list drawing-board"
+          >
+            {items}
+          </Draggable>
+        )
+      }
+    }
+    const renderInner = (field: FormField) => {
+      return field.children?.map((el: FormField) => {
+        return (
+          <JsonRender
+            key={el.props.key}
+            field={el}
+            modelValue={el.value}
+            onUpdate:modelValue={(val) => (el.value = val)}
+          >
+            {{
+              default: () => renderChildren(el)
+            }}
+          </JsonRender>
+        )
+      })
+    }
+    return () => {
+      const { field } = props
+      return (
+        <div class="drawing-board">
+          {renderItem({
+            field,
+            index: 0,
+            list: [],
+            parent: field
+          })}
+        </div>
+      )
+    }
+  }
+})
